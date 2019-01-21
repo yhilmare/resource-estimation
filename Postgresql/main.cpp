d6fe66e86a3b2afd963c610ebdd0281576cfda4b
root <yh_swjtu@163.com>
2019-01-21 22:36:56 +0800

    update at Jan 21st

--- Postgresql/main.cpp
+++ Postgresql/main.cpp
@@ -3,14 +3,20 @@
 
 int main() {
     using namespace std;
+    const char *conn_info = "postgresql://ilmare@10.69.35.174:5432/testdb?connect_timeout=10&password=123456";
     const char *conInfo = "host=10.69.35.174 port=5432 dbname=testdb connect_timeout=10 password=123456 user=ilmare";
-    PGconn *conn = PQconnectdb(conInfo);
+    PGconn *conn = PQconnectdb(conn_info);
+    try{
+        cout << PQparameterStatus(conn, "integer_datetimes") << endl;
+    }catch(const exception &e){
+        cout << e.what() << endl;
+    }
     if (PQstatus(conn) != CONNECTION_OK){
         cout << "Connection failed!" << endl;
         PQfinish(conn);
         exit(-1);
     }
-    const char *sql = "select * from test";
+    const char *sql = "explain select * from blog_visit limit 30";
     PGresult *result_set = PQexec(conn, sql);
     int column_count = PQnfields(result_set);
     int row_count = PQntuples(result_set);
