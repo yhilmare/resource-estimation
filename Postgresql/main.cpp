33d975c6bfa140113d30df75b07b731d0ebe1736
root <yh_swjtu@163.com>
2019-01-22 22:16:42 +0800

    update at Jan 22nd

--- Postgresql/main.cpp
+++ Postgresql/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include <libpq-fe.h>
+#include <string>
+#include <iterator>
+#include <vector>
+#include <unistd.h>
 
-int main() {
+void postgresql_test(){
     using namespace std;
-    const char *conn_info = "postgresql://ilmare@10.69.35.174:5432/testdb?connect_timeout=10&password=123456";
+    const char *conn_info = "postgresql://ilmare@10.69.35.174:5432/tpch?connect_timeout=10&password=123456";
     const char *conInfo = "host=10.69.35.174 port=5432 dbname=testdb connect_timeout=10 password=123456 user=ilmare";
     PGconn *conn = PQconnectdb(conn_info);
     try{
@@ -16,7 +21,7 @@ int main() {
         PQfinish(conn);
         exit(-1);
     }
-    const char *sql = "explain select * from blog_visit limit 30";
+    const char *sql = "select count(*) from lineitem";
     PGresult *result_set = PQexec(conn, sql);
     int column_count = PQnfields(result_set);
     int row_count = PQntuples(result_set);
@@ -29,5 +34,19 @@ int main() {
     }
     PQclear(result_set);
     PQfinish(conn);
+}
+
+int main(int arg_n, char *arg_v[]) {
+    using namespace std;
+    postgresql_test();
+//    cout << "pid is " << getpid() << ", ppid is " << getppid() << endl;
+//
+//    const char *process_path = "/bin/cat";
+//    int ret = execl(process_path, "cat", "/home/ilmare/Desktop/tpch-dbgen/pg_data/nation.tbl", NULL);
+//    cout << ret << endl;
+//    if (ret == -1){
+//        cerr << "Process init error" << endl;
+//    }
+//    cout << "Hello,world" << endl;
     return 0;
 }
