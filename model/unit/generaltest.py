24ec9edaae68049f4858f423aab2d3a007411b38
yhswjtuilmare <yh_swjtu@163.com>
2019-06-02 21:37:13 +0800

    update at Jun 2nd

--- model/unit/generaltest.py
+++ model/unit/generaltest.py
@@ -54,41 +54,4 @@ def pca_plot():
         fp.close()
 
 if __name__ == "__main__":
-    try:
-        fp = open(r"F:\resource_estimation\data\rnn\train.csv", "r")
-        fp1 = open(r"F:\resource_estimation\data\bp1\train.csv", "r")
-        reader = csv.reader(fp)
-        mpl.rcParams["xtick.labelsize"] = 8
-        mpl.rcParams["ytick.labelsize"] = 8
-        y = []
-        for idx, line in enumerate(reader):
-            y.append(int(line[-1]))
-            if idx >= 500000:
-                break
-        reader = csv.reader(fp1)
-        y1 = []
-        for idx, line in enumerate(reader):
-            y1.append(int(line[-1]))
-            if idx >= 500000:
-                break
-        x = np.arange(0, len(y))
-        fig = plt.figure("test")
-        ax = fig.add_subplot(311)
-        y = np.array(y, dtype=np.float32)
-        ax.plot(x, y, '.', label="concurrent")
-        ax.legend()
-        bx = fig.add_subplot(312)
-        y1 = np.array(y1, dtype=np.float32)
-        bx.plot(x, y1, '.', color="orange", label="non-concurrent")
-        bx.legend()
-        cx = fig.add_subplot(313)
-        cx.plot(x, y, '.', label="concurrent")
-        cx.plot(x, y1, '.', color="orange", label="non-concurrent")
-        cx.legend()
-        plt.show()
-    except Exception as e:
-        print(e)
-    finally:
-        fp.close()
-        fp1.close()
-    # plot_data(r"F:\resource_estimation\data\rnn1\train.csv")
+    plot_data(r"F:\resource_estimation\data\rnn\train.csv")
