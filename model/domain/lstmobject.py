5cb45d2cf721b83cdb95cb7d4150f79c5ac4814a
yhswjtuilmare <yh_swjtu@163.com>
2019-06-06 12:00:42 +0800

    update at Jun 6th

--- model/domain/lstmobject.py
+++ model/domain/lstmobject.py
@@ -8,6 +8,15 @@ import re
 import csv
 from sklearn.decomposition import PCA
 
+def generate_label(label, dim):
+    tmp_label = int(label)
+    result = [0 for _ in range(dim)]
+    if tmp_label <= dim - 2:
+        result[tmp_label] = 1
+    else:
+        result[len(result) - 1] = 1
+    return result
+
 class data_obj:
     def __init__(self, samples, labels, tran_size):
         self._samples = samples
@@ -33,7 +42,7 @@ class data_obj:
         return self._samples[idxs], self._labels[idxs]
 
 class lstm_data:
-    def __init__(self, pre_path, transaction_size):
+    def __init__(self, pre_path, transaction_size, label_dim, one_hot=False):
         test = re.match(r"^([a-zA-Z]:){0,1}([\\/][a-zA-Z0-9_-]+)+[\\/]{0,1}$", pre_path)
         assert test != None, Exception("path is invaild")
         if pre_path[-1] is not "\\" and pre_path[-1] is not "/":
@@ -41,6 +50,8 @@ class lstm_data:
         self._train_path = "{0}{1}".format(pre_path, "train.csv")
         self._test_path = "{0}{1}".format(pre_path, "test.csv")
         self._tran_size = transaction_size
+        self._one_hot = one_hot
+        self._label_dim = label_dim
         def parse_data(path):
             samples = []
             labels = []
@@ -48,11 +59,16 @@ class lstm_data:
             reader = csv.reader(fp)
             for line in reader:
                 samples.append(line[0: -1])
-                labels.append([line[-1]])
+                if self._one_hot:
+                    labels.append(generate_label(line[-1], self._label_dim))
+                else:
+                    self._label_dim = 1
+                    labels.append([line[-1]])
             fp.close()
             return np.array(samples, dtype=np.float32), np.array(labels, dtype=np.float32)
         self._train, self._train_labels = parse_data(self._train_path)
         self._test, self._test_labels = parse_data(self._test_path)
+        self._dim = self._train.shape[1]
         self.init_samples()
     def init_samples(self):
         self._train_samples = data_obj(self._train, self._train_labels, self._tran_size)
@@ -62,22 +78,34 @@ class lstm_data:
             self.__get = get
         def __get__(self, instance, owner):
             return self.__get(instance)
-    def get_train(self):
+    def __get_train(self):
         return self._train_samples
-    train = descriptor(get_train)
-    def get_test(self):
+    train = descriptor(__get_train)
+    def __get_test(self):
         return self._test_samples
-    test = descriptor(get_test)
+    test = descriptor(__get_test)
+    @property
+    def transaction_size(self):
+        return self._tran_size
+    @property
+    def sample_dim(self):
+        return self._dim
+    @property
+    def label_dim(self):
+        return self._label_dim
     def pca_samples(self, n_components):
         pca = PCA(n_components=n_components)
         self._train = pca.fit_transform(self._train)
         self._test = pca.fit_transform(self._test)
+        self._dim = n_components
         self.init_samples()
 
-
 if __name__ == "__main__":
-    obj = lstm_data(r"F:/resource_estimation/data/rnn", 28)
-    obj.pca_samples(8)
-    samples, labels = obj.train.next_batch(100)
-    print(samples.shape)
-    print(labels.shape)
+    obj = lstm_data(r"F:/resource_estimation/data/rnn", 25, 20, False)
+    labels = obj.test.labels
+    total = labels.shape[0] * labels.shape[1]
+    count = 0;
+    for num in labels.flatten():
+        if num == 0:
+            count += 1
+    print(count / total)
--- model/lib/lstm.py
+++ model/lib/lstm.py
@@ -6,12 +6,9 @@ Created By ILMARE
 from tensorflow.contrib import rnn
 import tensorflow as tf
 import time
-import os
 from domain.lstmobject import lstm_data
-import shelve
-from matplotlib import pyplot as plt
-import matplotlib as mpl
 import numpy as np
+import re
 
 def pick_top_n(preds, vocab_size, top_n=5):
     p = np.squeeze(preds)
@@ -21,57 +18,55 @@ def pick_top_n(preds, vocab_size, top_n=5):
     return c
 
 class lstm_model:
-    def __init__(self, hidden_size, num_layer, corpus, keep_prob,
-                 embedding_size, lr, max_step, save_path, sampling=False):
+    def __init__(self, hidden_size, num_layer, data_obj, keep_prob,
+                 l_rate, max_step, save_path, batch_size, sampling=False):
         if not sampling:
-            self._num_seq = corpus.num_seq
-            self._num_step = corpus.num_step
+            self._batch_size = batch_size
+            self._tran_size = data_obj.transaction_size
         else:
-            self._num_seq = 1
-            self._num_step = 1
+            self._batch_size = 1
+            self._tran_size = 1
+        test = re.match(r"^([a-zA-Z]:){0,1}([\\/][a-zA-Z0-9_-]+)+[\\/]{0,1}$", save_path)
+        assert test != None, Exception("path is invaild")
+        if save_path[-1] is not "\\" and save_path[-1] is not "/":
+            save_path = "{0}/".format(save_path)
         self._save_path = save_path
-        self._lr = lr
+        self._lr = l_rate
         self._max_step = max_step
-        self._embedding_size = embedding_size
         self._hidden_size = hidden_size
         self._num_layer = num_layer
-        self._corpus = corpus
+        self._data_obj = data_obj
         self._keep_prob = keep_prob
         tf.reset_default_graph()
-        self.init_inputs()
         self.build_lstm()
         self.define_loss()
         self.define_gradients()
-    def init_inputs(self):
-        self._x = tf.placeholder(dtype=tf.int32,
-                                      shape=[self._num_seq, self._num_step])
-        with tf.device("/cpu:0"):
-            embedding = tf.get_variable("embedding",
-                                        shape=[self._corpus.word_num,
-                                               self._embedding_size], dtype=tf.float32)
-            self._inputs = tf.nn.embedding_lookup(embedding, self._x)
     def build_lstm(self):
         def build_cell():
             cell = rnn.BasicLSTMCell(self._hidden_size, forget_bias=1.0, state_is_tuple=True)
             cell = rnn.DropoutWrapper(cell, output_keep_prob=self._keep_prob)
             return cell
+        self._inputs = tf.placeholder(dtype=tf.float32,
+                                      shape=[self._batch_size, self._tran_size, self._data_obj.sample_dim])
         mul_cell = rnn.MultiRNNCell([build_cell() for _ in range(self._num_layer)],
                                     state_is_tuple=True)
-        self._init_state = mul_cell.zero_state(self._num_seq, dtype=tf.float32)
+        self._init_state = mul_cell.zero_state(self._batch_size, dtype=tf.float32)
         outputs, self._final_state = tf.nn.dynamic_rnn(mul_cell, self._inputs,
                                                        initial_state=self._init_state)
-        print(outputs.shape)
+        # print(outputs.shape)
         outputs = tf.reshape(outputs, [-1, self._hidden_size])
-        W = tf.Variable(tf.truncated_normal([self._hidden_size, self._corpus.word_num],
+        W = tf.Variable(tf.truncated_normal([self._hidden_size, self._data_obj.label_dim],
                                             stddev=0.1, dtype=tf.float32))
-        bais = tf.Variable(tf.zeros([1, self._corpus.word_num],
+        bais = tf.Variable(tf.zeros([1, self._data_obj.label_dim],
                                     dtype=tf.float32), dtype=tf.float32)
         self._prediction = tf.nn.softmax(tf.matmul(outputs, W) + bais)
     def define_loss(self):
-        self._y = tf.placeholder(dtype=tf.int32, shape=[self._num_seq, self._num_step])
-        y_one_hot = tf.reshape(tf.one_hot(self._y, self._corpus.word_num), self._prediction.shape)
+        self._y = tf.placeholder(dtype=tf.float32, shape=[self._batch_size, self._tran_size, self._data_obj.label_dim])
+        y_one_hot = tf.reshape(self._y, self._prediction.shape)
         self._loss = -tf.reduce_mean(tf.reduce_sum(y_one_hot * tf.log(self._prediction),
                                                    reduction_indices=[1]))
+        self._accuracy = tf.reduce_mean(
+            tf.cast(tf.equal(tf.argmax(self._prediction, 1), tf.argmax(y_one_hot, 1)), dtype=np.float32))
     def define_gradients(self):
         vars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(self._loss, vars), 3)
@@ -82,96 +77,30 @@ class lstm_model:
             return_mat = []
             sess.run(tf.global_variables_initializer())
             state = sess.run(self._init_state)
-            step = 0
             start = time.clock()
-            for x, y in self._corpus.generate_batch():
-                feed = {self._x: x,
-                        self._y: y,
-                        self._init_state:state}
-                loss, _, state = sess.run([self._loss, self._optimizer,
-                                           self._final_state], feed_dict = feed)
+            for step in range(self._max_step):
+                train, labels = self._data_obj.train.next_batch(self._batch_size)
+                feed = {self._inputs: train, self._y: labels, self._init_state:state}
+                loss, _, state, acc = sess.run([self._loss, self._optimizer,
+                                           self._final_state, self._accuracy], feed_dict=feed)
                 return_mat.append(loss)
-                step += 1
                 if step % 10 == 0:
                     end = time.clock()
                     interval = end - start
-                    yield return_mat
-                    print("迭代次数：{0:d}/{2:d}，当前损失：{1:.3f}，迭代速度：{3:.3f} 秒/十次，约需要{4:.3f}秒"
-                          .format(step, loss, self._max_step, interval, ((self._max_step - step) / 10) * interval))
+                    print("迭代次数：{0:d}/{2:d}，当前损失：{1:.3f}，准确率：{5:.3f}，迭代速度：{3:.3f} 秒/十次，约需要{4:.3f}秒".format(
+                        step, loss, self._max_step, interval, ((self._max_step - step) / 10) * interval, acc))
                     start = time.clock()
-                if step == self._max_step:
-                    break
             tf.train.Saver().save(sess, "{0}model".format(self._save_path), global_step=step)
     def load_model(self):
         sess = tf.Session()
         tf.train.Saver().restore(sess, tf.train.latest_checkpoint(self._save_path))
         self._sess = sess
-    def sampling(self, init_str, max_sample=30):
-        sample = [c for c in init_str]
-        pre = np.ones((self._corpus.word_num, ))
-        state = self._sess.run(self._init_state)
-        for c in sample:
-            feed = {self._x: np.reshape(c, [1, 1]),
-                    self._init_state: state}
-            pre, state = self._sess.run([self._prediction, self._final_state],
-                                 feed_dict=feed)
-        c = pick_top_n(pre, self._corpus.word_num)
-        sample.append(c)
-        for count in range(max_sample):
-            x = np.zeros([1, 1])
-            x[0][0] = c
-            feed = {self._x: x,
-                    self._init_state: state}
-            pre, state = self._sess.run([self._prediction, self._final_state],
-                                        feed_dict=feed)
-            c = pick_top_n(pre, self._corpus.word_num)
-            sample.append(c)
-        return sample
-
-path = r"F:/tensorflow/rnn/data/novel.txt"
-save_path = r"F:/tensorflow/rnn/model/"
-
-def train_model():
-    obj = lstm_data(path, 50, 50, 3000)
-    with shelve.open("{0}corpus".format(save_path)) as fp:
-        fp["obj"] = obj
-    model = lstm_model(hidden_size=128, num_layer=2,
-                     corpus=obj, keep_prob=1.0,
-                     embedding_size=128, max_step=5000,
-                     lr=0.005, save_path=save_path)
-    result = []
-    fig = plt.figure("cross-entropy")
-    mpl.rcParams['xtick.labelsize'] = 8
-    mpl.rcParams['ytick.labelsize'] = 8
-    ax = fig.add_subplot(111)
-    # ax.grid(True)
-    for return_mat in model.train():
-        result.extend(return_mat)
-        # x = np.arange((len(return_mat)))
-        # y = np.array(return_mat)
-        # ax.plot(x, y, linewidth=0.8, color="b")
-        # plt.pause(0.1)
-    x = np.arange((len(return_mat)))
-    y = np.array(return_mat)
-    ax.plot(x, y, linewidth=0.8, color="b")
-    plt.show()
-
-def test_model(init_str, max_sample=200):
-    obj = None
-    assert os.path.exists("{0}corpus.bak".format(save_path)), "找不到文件"
-    with shelve.open("{0}corpus".format(save_path)) as fp:
-        obj = fp["obj"]
-    model = lstm_model(hidden_size=128, num_layer=2,
-                     corpus=obj, keep_prob=1.0,
-                     embedding_size=128, max_step=5000,
-                     lr=0.005, save_path=save_path, sampling=True)
-    model.load_model()
-    sample = model.sampling(obj.sentence_to_int(init_str), max_sample)
-    print(obj.int_to_sentence(sample))
 
 if __name__ == "__main__":
-    obj = lstm_data(path, 50, 50, 3000)
-    model = lstm_model(hidden_size=128, num_layer=2,
-                       corpus=obj, keep_prob=1.0,
-                       embedding_size=128, max_step=5000,
-                       lr=0.005, save_path=save_path)
+    model_path = r"F:/resource_estimation/model/rnn/"
+    obj = lstm_data(r"F:/resource_estimation/data/rnn", 25, 20, True)
+    obj.pca_samples(8)
+    model = lstm_model(hidden_size=128, num_layer=2, data_obj=obj,
+                       keep_prob=0.8, l_rate=0.005, max_step=5000,
+                       save_path=model_path, batch_size=256)
+    model.train()
