b5ac6fe5bed57f3eb30cb76c5e957d85154b12bb
yhswjtuilmare <yh_swjtu@163.com>
2019-07-09 11:01:49 +0800

    update at July 9th

--- load/main.cpp
+++ load/main.cpp
@@ -60,37 +60,36 @@ void load_data(){
 
 int main(int argn, char *argv[]) {
     using namespace std;
-
-    SYSTEMTIME st;
-    GetLocalTime(&st);
-
-    time_t time_stamp = time(NULL);
-    string tmp;
-    if (st.wMilliseconds < 100){
-        if (st.wMilliseconds < 10){
-            tmp = "00" + parseInt(st.wMilliseconds);
-        }else{
-            tmp = "0" + parseInt(st.wMilliseconds);
-        }
-    }else{
-        tmp = parseInt(st.wMilliseconds);
-    }
-    string final = parseInt(time_stamp) + tmp;
-    char buffer[1000];
-    getcwd(buffer, 1000);
-    unordered_map<string, string> config =
-            parse_properties_file(string(buffer) + "/config/pg_config.properties");
-    string file_name = config["DATA_FILE"] + "originlog_" + final + ".csv";
-
-    int thread_num = 10;
-    thread_arg arg(config, thread_num, file_name);
-
-    for (int i = 0; i < thread_num - 1; i ++){
-        pthread_t t1;
-        pthread_create(&t1, NULL, thread_main, (void *) &arg);
-//        pthread_join(t1, NULL);
-    }
-    thread_main((void *)&arg);
+//    SYSTEMTIME st;
+//    GetLocalTime(&st);
+//
+//    time_t time_stamp = time(NULL);
+//    string tmp;
+//    if (st.wMilliseconds < 100){
+//        if (st.wMilliseconds < 10){
+//            tmp = "00" + parseInt(st.wMilliseconds);
+//        }else{
+//            tmp = "0" + parseInt(st.wMilliseconds);
+//        }
+//    }else{
+//        tmp = parseInt(st.wMilliseconds);
+//    }
+//    string final = parseInt(time_stamp) + tmp;
+//    char buffer[1000];
+//    getcwd(buffer, 1000);
+//    unordered_map<string, string> config =
+//            parse_properties_file(string(buffer) + "/config/pg_config.properties");
+//    string file_name = config["DATA_FILE"] + "originlog_" + final + ".csv";
+//
+//    int thread_num = 10;
+//    thread_arg arg(config, thread_num, file_name);
+//
+//    for (int i = 0; i < thread_num - 1; i ++){
+//        pthread_t t1;
+//        pthread_create(&t1, NULL, thread_main, (void *) &arg);
+////        pthread_join(t1, NULL);
+//    }
+//    thread_main((void *)&arg);
     return 0;
 }
 
